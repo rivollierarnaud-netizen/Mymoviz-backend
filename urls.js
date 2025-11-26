aa66a93fda43c75729139530c972f299e26e35b0
rivollierarnaud-netizen <rivollierarnaud@gmail.com>
2025-11-26 19:59:07 +0100

    vercel2

--- urls.js
+++
@@ -1,7 +0,0 @@
-// Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://mymoviz-backend-three-inky.vercel.app';
-
-// Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://mymoviz-frontend-opal-mu.vercel.app';
-
-module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
